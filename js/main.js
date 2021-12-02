46a8df75c28c04b32ab150cb2942a5c2288b9e44
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2021-12-02 20:09:32 +0300

    Bug fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var scrollAnimRunning = false;
 
+window.addEventListener("wheel", e => e.preventDefault(), { passive:false });
+
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
@@ -7,25 +9,34 @@ function delay(time) {
 async function onLoad() {
     await delay(500);
     $(".fade").fadeIn(1000);
+
+    await delay(1000);
+    window.addEventListener('wheel', scrollHandler);
 }
 
-window.addEventListener('wheel', function(e){
+function scrollHandler(e) {
     if(!scrollAnimRunning) {
         if (e.deltaY >= 0) { //DOWN
-            scrollAnimRunning = true;
-            $('html, body').animate({ scrollTop: $(".links_container").offset().top }, 2000);
-            $('html, body').promise().done(function(){ scrollAnimRunning = false;});
+            scrollDown();
         }
         else { //UP
-            scrollAnimRunning = true;
-            $('html, body').animate({ scrollTop: $(".head").offset().top }, 2000);
-            $('html, body').promise().done(function(){ scrollAnimRunning = false;});
+            scrollUp();
         }
     }
-});
+}
 
 function scrollDown(){
     scrollAnimRunning = true;
     $('html, body').animate({ scrollTop: $(".links_container").offset().top }, 2000);
     $('html, body').promise().done(function(){ scrollAnimRunning = false;});
+}
+
+function scrollUp() { 
+    scrollAnimRunning = true;
+    $('html, body').animate({ scrollTop: $(".head").offset().top }, 2000);
+    $('html, body').promise().done(function(){ scrollAnimRunning = false;});
+}
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
 }
