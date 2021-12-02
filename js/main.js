9b8278277839dd23fb38abef8ffc73702f2da848
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2021-12-02 19:41:15 +0300

    Fixed bug when mouse wheel scrolling

--- js/main.js
+++ js/main.js
@@ -9,16 +9,18 @@ async function onLoad() {
     $(".fade").fadeIn(1000);
 }
 
-$(window).bind('mousewheel', function(e) {
+window.addEventListener('wheel', function(e){
     if(!scrollAnimRunning) {
-        if (e.originalEvent.wheelDelta >= 0) { //UP
-            scrollAnimRunning = true;
-            $('html, body').animate({ scrollTop: $(".head").offset().top }, 2000);
-            $('html, body').promise().done(function(){ scrollAnimRunning = false;});
-        }
-        else { //DOWN
+        if (e.deltaY >= 0) { //DOWN
+            console.log("Down");
             scrollAnimRunning = true;
             $('html, body').animate({ scrollTop: $(".links_container").offset().top }, 2000);
+            $('html, body').promise().done(function(){ scrollAnimRunning = false;});
+        }
+        else { //UP
+            console.log("Up");
+            scrollAnimRunning = true;
+            $('html, body').animate({ scrollTop: $(".head").offset().top }, 2000);
             $('html, body').promise().done(function(){ scrollAnimRunning = false;});
         }
     }
