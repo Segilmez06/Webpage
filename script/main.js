35f7fa735fdc5187bb2061a79e7e5bc987d3046c
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2024-09-12 22:33:15 +0300

    Add WearOS page

--- script/main.js
+++ script/main.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', pageLoaded);
 
+let GalaxyWatches = [
+    "SM-R860", // 4
+    "SM-R870", // 4
+    "SM-R880", // 4 Classic
+    "SM-R890", // 4 Classic
+    "SM-R900", // 5
+    "SM-R910", // 5
+    "SM-R920", // 5 Pro
+    "SM-R930", // 6
+    "SM-R940", // 6
+    "SM-R950", // 6 Classic
+    "SM-L300", // 7
+    "SM-L310", // 7
+    "SM-L705F" // Ultra
+];
+
 function pageLoaded(){
+    for (let i = 0; i < GalaxyWatches.length; i++) {
+        if (navigator.userAgent.toUpperCase().includes(GalaxyWatches[i])) {
+            window.location.href = "/GalaxyWatch";
+            break;
+        }
+    }
+
     document.querySelectorAll(".links-box > a:not([href])").forEach((e) => {
         e.addEventListener("click", () => { alert("Sorry, this page is not ready yet!\nStay tuned, work in progress..."); });
     });
